052d98634edc79b005ba635afd0aa2740d167ebb
Angel <duconguaidia@gmail.com>
2026-06-05 17:20:42 -0500

    Delete Api_Crud_Angel directory

--- Api_Crud_Angel/api/mixins.py
+++
@@ -1,5 +0,0 @@
-class SoftDeleteMixin:
-
-    def perform_destroy(self, instance):
-        instance.activo = False
-        instance.save()
--- Api_Crud_Angel/run.py
+++
@@ -1,9 +0,0 @@
-import os
-from decouple import config
-
-# Leer el puerto .env
-port = config('API_PORT')
-
-# Ejecutar el servidor django
-
-os.system(f'python manage.py runserver {port}')
